c822e9ca4ae0deeb1cdb213ee7cb93de86dfb054
Lukasz17git <lucas.m2295@gmail.com>
2024-01-02 18:59:56 +0100

    new release

--- src/utilities/remove.ts
+++ src/utilities/remove.ts
@@ -1,3 +1,4 @@
+import { isObjectLiteral } from "./isObjectLiteral";
 
 
 /** Returns only the optional keys of an object. */
@@ -5,12 +6,13 @@ type OptionalKeys<T> = { [K in keyof T]-?: {} extends Pick<T, K> ? K : never }[k
 
 
 /** Removes properties or indexes, order doesn't matter. */
-type Remove = <T extends object>(
+type Remove = <T extends Record<PropertyKey, any>>(
    value: T,
    keys: T extends any[] ? (number | `${number}` | (number | `${number}`)[])
       : OptionalKeys<T> | (OptionalKeys<T>)[]
 ) => T
-
+//TODO: Improve the typing to remove the fields choosen, maybe split it into two different function overloading,
+// one for arrays and the other one for objects
 
 /**
  * Removes properties or indexes, order doesn't matter.
@@ -20,7 +22,7 @@ type Remove = <T extends object>(
  */
 export const remove: Remove = (value, keys): typeof value => {
 
-   if (!Array.isArray(value) && value?.constructor !== Object) throw 'Wrong type provided in "remove" function'
+   if (!Array.isArray(value) && !isObjectLiteral(value)) throw 'Wrong type provided in "remove" function'
 
    let keysAsArray = Array.isArray(keys) ? keys : [keys]
 
